3a5b1810946bb0ab59c7fc0a153751789a19af1c
Brillywam <brilliantmwendwa@gmail.com>
2024-04-05 22:28:52 +0300

    first commit

---
+++ Decisionmaking.dart
@@ -0,0 +1,13 @@
+//Decision making statements; if statement outputs depend on certain conditional expressions
+void main() {
+  var age = 1;
+  if (age > 18) {
+    print ("Mariam is a voter in Kenya");
+  } else if (age ==18) {
+    print ("Mariam just became eligible to vote in Kenya");
+    } else if (age <=16) {
+      print ("Mariam is still young to vote");
+    } else {
+    print ("Mariam should not vote");
+  }
+}
---
+++ loopingStatements.dart
@@ -0,0 +1,40 @@
+//dart for loop
+// void main () {
+// int num = 1;
+// for (num; num<=19; num++)
+// {
+//   print(num);
+// }
+// }
+//dart for... in loop
+// void main ()
+// {
+//   var list1= [10,20,30,40,5];
+//   for(var i in list1)
+//   {
+//     print(i);
+//   }
+// }
+//dart while loop
+// void main()
+// {
+//   var a = 2;
+//   var maxnum = 20;
+//   while(a<maxnum){
+//     print(a);
+//     a = a+2;
+//   }
+// }
+//Dart do-while loop
+// void main()
+// {
+//   var a = 1;
+//   var maxnum = 100;
+//   do
+//   {
+//     print("The value is: ${a}");
+//     a = a*2;
+//   }
+//   while(a<maxnum);
+// }
+
